38582d9ac13df8027ed86ecfe561034b2a6d668b
Jacob Johnson <jacob.johnson@3cloudsolutions.com>
2024-07-25 20:46:38 -0500

    Push latest changes

--- webhooks/approve-deployment.sh
+++ webhooks/approve-deployment.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-# Variables
-# Read JSON payload from webhook
-read -r WEBHOOK_PAYLOAD
 
-# Parse JSON payload to extract values
-DEPLOYMENT_ID=$(echo "$WEBHOOK_PAYLOAD" | jq -r '.deployment.id')
-ENVIRONMENT_NAME=$(echo "$WEBHOOK_PAYLOAD" | jq -r '.deployment.environment')
-REPOSITORY_NAME=$(echo "$WEBHOOK_PAYLOAD" | jq -r '.repository.name')
-REPOSITORY_OWNER=$(echo "$WEBHOOK_PAYLOAD" | jq -r '.repository.owner.login')
-INSTALLATION_ID=$(echo "$WEBHOOK_PAYLOAD" | jq -r '.installation.id')
+# Read arguments
+DEPLOYMENT_URL=$1
+ENVIRONMENT_NAME=$2
+INSTALLATION_ID=$3
+GITHUB_EVENT=$4
+
+# Debug: Print the extracted values
+echo "Deployment URL: $DEPLOYMENT_URL"
+echo "Environment Name: $ENVIRONMENT_NAME"
+echo "Installation ID: $INSTALLATION_ID"
 
 APP_ID="APP_ID"
 PRIVATE_KEY_PATH="/path/to/private-key.pem"
@@ -34,19 +35,40 @@ SIGNATURE=$(echo -n "$HEADER_PAYLOAD" | openssl dgst -sha256 -sign $PRIVATE_KEY_
 # Combine header, payload, and signature to form the JWT
 JWT="$HEADER_PAYLOAD.$SIGNATURE"
 
+echo
 echo $JWT
+echo
 
 echo Auth as GitHub App and get installation access token
 INSTALLATION_ACCESS_TOKEN=$(curl -s -X POST \
   -H "Authorization: Bearer $JWT" \
   -H "Accept: application/vnd.github+json" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/app/installations/$INSTALLATION_ID/access_tokens | jq -r .token)
+  https://api.github.com/app/installations/$INSTALLATION_ID/access_tokens | jq -r '.token')
 
-echo approve deployment job
-curl -X POST \
-  -H "Authorization: Bearer $INSTALLATION_ACCESS_TOKEN" \
+echo
+echo $INSTALLATION_ACCESS_TOKEN
+echo
+
+echo wait...
+sleep 15
+echo
+echo running approval on this url
+echo $DEPLOYMENT_URL
+echo
+
+echo approving deployment job
+curl -L \
+  -X POST \
   -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $INSTALLATION_ACCESS_TOKEN" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  -d '{"state": "approved"}' \
-  https://api.github.com/repos/$REPOSITORY_OWNER/$REPOSITORY_NAME/deployments/$DEPLOYMENT_ID/environments/$ENVIRONMENT_NAME/approval
+  $DEPLOYMENT_URL \
+  -d '{
+    "environment_name": "'"$ENVIRONMENT_NAME"'",
+    "state": "approved",
+    "comment": "LGTM"
+  }'
+
+echo
+echo deployment job approved!
